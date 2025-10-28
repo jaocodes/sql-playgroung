115a088b54452c100a4fc22b1d63ad12eddc212d
jaocodes <jhonskaz@gmail.com>
2025-10-28 11:02:33 -0300

    praticando SubQuery

--- 19_subquery.sql
+++ 19_subquery.sql
@@ -36,3 +36,13 @@ WHERE substr(DtCriacao, 1,10) = '2025-08-25'
 ) 
 AND
 substr(t1.DtCriacao,1,10) = '2025-08-29';
+
+
+SELECT *
+FROM (
+    SELECT * 
+    FROM transacoes
+    WHERE DtCriacao >= '2025-01-01'
+)
+WHERE DtCriacao < '2025-07-01'
+
