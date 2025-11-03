694c814e32eeff32b56106e8dd290d30c5e76f0b
jaocodes <jhonskaz@gmail.com>
2025-11-03 15:40:19 -0300

    exemplo de funcao lag em Window Function, aula 26

--- 24_window_function_2.sql
+++ 24_window_function_2.sql
@@ -45,3 +45,35 @@ FROM tb_lag;
 
 
 
+-- Em média, em quantos dias os usuários voltam ao curso em 2025?
+
+
+WITH cliente_dia AS (
+        SELECT DISTINCT idCliente,
+                substr(DtCriacao,1,10) AS dtDia
+        FROM transacoes
+        WHERE substr(DtCriacao,1,4) = '2025'
+        ORDER BY idCliente, dtDia
+),
+tb_lag AS (
+        SELECT *,
+                lag(dtDia) OVER (PARTITION BY idCliente ORDER BY dtDia) AS lagDtDia
+
+        FROM cliente_dia
+), tb_diff_dt AS (
+
+        SELECT *,
+                julianday(dtDia) - julianday(lagDtDia) AS dtDiff
+
+        FROM tb_lag
+), avg_cliente AS (
+
+        SELECT idCliente, avg(dtDiff) AS avgDia
+
+        FROM tb_diff_dt
+        GROUP BY idCliente
+) 
+
+SELECT avg(avgDia) FROM avg_cliente;
+
+
